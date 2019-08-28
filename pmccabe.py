2e52e9e70ceb3d9fbf09d6df5125fb146cf3e15b
Mike Mallin <mike@mremallin.ca>
2019-08-28 01:07:46 -0400

    Create output panel

--- pmccabe.py
+++ pmccabe.py
@@ -5,8 +5,11 @@ import os
 
 class PmccabeCommand(sublime_plugin.WindowCommand):
     def run(self):
-        view = self.view
-        code_text = view.substr(0, view.size())
+        view = self.window.active_view()
+        code_text = view.substr(sublime.Region(0, view.size()))
+
+        output_panel = self.window.create_output_panel("pmccabe")
+        self.window.run_command("show_panel", {"panel": "output.pmccabe"})
 
     def is_enabled(self):
         s = sublime.load_settings("Preferences.sublime-settings")
