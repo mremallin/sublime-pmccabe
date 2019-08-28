e836457f2198e16c6eca285beaddbbcd6d4c96aa
Mike Mallin <mike@mremallin.ca>
2019-08-28 18:40:13 -0400

    Ported more async infrastructure

--- pmccabe.py
+++ pmccabe.py
@@ -2,6 +2,13 @@ import sublime
 import sublime_plugin
 
 import os
+import subprocess
+import sys
+import threading
+import time
+import codecs
+import signal
+import collections
 from subprocess import Popen, PIPE
 
 class ProcessListener(object):
@@ -11,15 +18,132 @@ class ProcessListener(object):
     def on_finished(self, proc):
         pass
 
+class AsyncProcess(object):
+    def __init__(self, executable, file_path, listener):
+        if not file_path:
+            raise ValueError("Need a file to analyze")
+
+        self.listener = listener
+        self.killed = False
+        self.start_time = time.time()
+
+        # Hide the console window on Windows
+        startupinfo = None
+        if os.name == "nt":
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        if sys.platform == "win32":
+            # Use shell=True on Windows, so shell_cmd is passed through with the correct escaping
+            self.proc = subprocess.Popen(
+                shell_cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                startupinfo=startupinfo,
+                env=proc_env,
+                shell=True)
+        elif sys.platform == "darwin":
+            # Use a login shell on OSX, otherwise the users expected env vars won't be setup
+            self.proc = subprocess.Popen(
+                ["/usr/bin/env", "bash", "-l", "-c", executable, "-v", file_path],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                startupinfo=startupinfo,
+                env=proc_env,
+                preexec_fn=preexec_fn,
+                shell=False)
+        elif sys.platform == "linux":
+            # Explicitly use /bin/bash on Linux, to keep Linux and OSX as
+            # similar as possible. A login shell is explicitly not used for
+            # linux, as it's not required
+            self.proc = subprocess.Popen(
+                ["/usr/bin/env", "bash", "-c", shell_cmd, executable, "-v", file_path],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                startupinfo=startupinfo,
+                env=proc_env,
+                preexec_fn=preexec_fn,
+                shell=False)
+
+        if self.proc.stdout:
+            threading.Thread(
+                target=self.read_fileno,
+                args=(self.proc.stdout.fileno(), True)
+            ).start()
+
+        if self.proc.stderr:
+            threading.Thread(
+                target=self.read_fileno,
+                args=(self.proc.stderr.fileno(), False)
+            ).start()
+
+    def kill(self):
+        if not self.killed:
+            self.killed = True
+            if sys.platform == "win32":
+                # terminate would not kill process opened by the shell cmd.exe,
+                # it will only kill cmd.exe leaving the child running
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+                subprocess.Popen(
+                    "taskkill /PID %d /T /F" % self.proc.pid,
+                    startupinfo=startupinfo)
+            else:
+                os.killpg(self.proc.pid, signal.SIGTERM)
+                self.proc.terminate()
+            self.listener = None
+
+    def poll(self):
+        return self.proc.poll() is None
+
+    def exit_code(self):
+        return self.proc.poll()
+
+    def read_fileno(self, fileno, execute_finished):
+        decoder_cls = codecs.getincrementaldecoder(self.listener.encoding)
+        decoder = decoder_cls('replace')
+        while True:
+            data = decoder.decode(os.read(fileno, 2**16))
+
+            if len(data) > 0:
+                if self.listener:
+                    self.listener.on_data(self, data)
+            else:
+                try:
+                    os.close(fileno)
+                except OSError:
+                    pass
+                if execute_finished and self.listener:
+                    self.listener.on_finished(self)
+                break
+
 class PmccabeCommand(sublime_plugin.WindowCommand, ProcessListener):
+    text_queue = collections.deque()
+    text_queue_proc = None
+    text_queue_lock = threading.Lock()
+
     def _get_pmccabe_executable(self):
         s = sublime.load_settings("pmccabe.sublime-settings")
         pmccabe_executable = s.get("pmccabe_executable", "/usr/bin/pmccabe")
         return pmccabe_executable
 
-    def run(self):
-        view = self.window.active_view()
+    def run(self, kill=False, **kwargs):
+        # clear the text_queue
+        with self.text_queue_lock:
+            self.text_queue.clear()
+            self.text_queue_proc = None
 
+        if kill:
+            if self.proc:
+                self.proc.kill()
+                self.proc = None
+                self.append_string(None, "[Cancelled]")
+            return
+
+        view = self.window.active_view()
         self.output_panel = self.window.create_output_panel("pmccabe")
         self.window.run_command("show_panel", {"panel": "output.pmccabe"})
 
@@ -29,10 +153,38 @@ class PmccabeCommand(sublime_plugin.WindowCommand, ProcessListener):
         self.output_panel.run_command('append', 
             {'characters': pmccabe_stdout, 'force': True, 'scroll_to_end': True})
 
-    def is_enabled(self):
+    def is_enabled(self, kill=False, **kwargs):
+        if kill:
+            return (self.proc is not None) and self.proc.poll()
+
         pmccabe_executable = self._get_pmccabe_executable()
         if not os.path.exists(pmccabe_executable):
             sublime.error_message("The pmccabe executable provided at '{}' does not exist".format(pmccabe_executable))
             return False
 
         return True
+
+    def append_string(self, proc, str):
+        was_empty = False
+        with self.text_queue_lock:
+            if proc != self.text_queue_proc and proc:
+                # a second call to exec has been made before the first one
+                # finished, ignore it instead of intermingling the output.
+                proc.kill()
+                return
+
+            if len(self.text_queue) == 0:
+                was_empty = True
+                self.text_queue.append("")
+
+            available = self.BLOCK_SIZE - len(self.text_queue[-1])
+
+            if len(str) < available:
+                cur = self.text_queue.pop()
+                self.text_queue.append(cur + str)
+            else:
+                self.text_queue.append(str)
+
+        if was_empty:
+            sublime.set_timeout(self.service_text_queue, 0)
+
